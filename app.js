b114385bc3776b18a40d86a565fe54391734be5d
Gavin Fungtammasan <gavin.kiwi@gmail.com>
2021-04-30 19:45:07 +1000

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -10,9 +11,9 @@ var usersRouter = require('./routes/users');
 
 var mongoUri = process.env.MONGO_URI;
 
-console.log(mongoUri);
-
 var app = express();
+
+
 
 const options = {
   useNewUrlParser: true
@@ -40,6 +41,7 @@ db.once("open", () => console.log("Successfully connected to mongo"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
