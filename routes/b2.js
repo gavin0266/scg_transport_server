9b01f2dee10024b69e72be4226fab27417ca95cb
Gavin Fungtammasan <gavin.kiwi@gmail.com>
2021-05-11 00:54:33 +1000

    add surge frontend endpoint

--- routes/b2.js
+++ routes/b2.js
@@ -1,13 +1,20 @@
 const getAuthDetails = require('../services/getAuthDetails');
 const getUploadDetails = require('../services/getUploadDetails');
 
+var clientUrl = "https://scg-af-logistic.surge.sh/";
+
+var debug = false;
+
+if(debug) {
+    clientUrl = "http://localhost:3001";
+}
 
 module.exports = async (req, res, next) => {
     try {
         const authDetails = await getAuthDetails();
         const uploadDetails = await getUploadDetails(authDetails);
 
-        res.set('Access-Control-Allow-Origin', 'http://localhost:3001');
+        res.set('Access-Control-Allow-Origin', clientUrl);
         res.json(uploadDetails);
         return;
     } catch (e) {
