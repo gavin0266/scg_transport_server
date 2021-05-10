2859d8b7a672a493f85eee402382904341d53e33
Gavin Fungtammasan <gavin.kiwi@gmail.com>
2021-05-11 01:16:30 +1000

    fix b2

--- routes/b2.js
+++ routes/b2.js
@@ -3,7 +3,7 @@ const getUploadDetails = require('../services/getUploadDetails');
 
 var clientUrl = "https://scg-af-logistic.surge.sh";
 
-var debug = true;
+var debug = false;
 
 if(debug) {
     clientUrl = "http://localhost:3001";
